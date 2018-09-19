ddd7f1cce522b7a341dd4471736aa28b1dac63be
JingzheLi <2365191668@qq.com>
2018-09-19 22:39:10 +0800

    第一个

---
+++ untitled.js
@@ -0,0 +1,13 @@
+var __WAWebviewStartTime__ = Date.now();
+
+function _toArray(e) {
+  return Array.isArray(e) ? e : Array.from(e)
+}
+
+function _toConsumableArray(e) {
+  if (Array.isArray(e)) {
+    for (var t = 0, n = Array(e.length); t < e.length; t++) n[t] = e[t];
+    return n
+  }
+  return Array.from(e)
+}
